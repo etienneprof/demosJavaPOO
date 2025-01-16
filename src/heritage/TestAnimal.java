f4e2dddd7d4fa2fcbc7974afbe44b4558bbfe612
excilyano <agahnim13@gmail.com>
2025-01-16 13:50:29 +0100

    transtypage et polymorphisme

--- src/heritage/TestAnimal.java
+++ src/heritage/TestAnimal.java
@@ -1,5 +1,8 @@
 package heritage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestAnimal {
 	public static void main(String[] args) {
 		Animal ori = new Animal("Ori");
@@ -29,5 +32,53 @@ public class TestAnimal {
 		System.out.println(tails);
 		System.out.println(bipbip);
 		System.out.println(titi);
+		
+		dessiner(ori);
+		dessiner(sonic);
+		dessiner(tails);
+		
+		// transtypage ascendant : mes instances sont de type "Herisson" et "Oiseau", mais je les stocke dans une liste
+		// de "Animal".
+		List<Animal> animaux = new ArrayList<Animal>();
+		animaux.add(sonic);
+		animaux.add(bipbip);
+		animaux.add(ori);
+		
+		// Polymorphisme : l'affichage de "current" utilise bien le toString de la classe la plus précise.
+		for (Animal current : animaux) {
+			System.out.println(current);
+		}
+		
 	}
+	
+	// transtypage ascendant : je généralise une instance initialement précise.
+	public static void dessiner(Animal animal) {
+		System.out.println("***********************************");
+		System.out.println(animal);
+		// transtype descendant : je pars d'une instance générique et je la traite comme une instace plus précise
+		if (animal instanceof Herisson) {
+			Herisson herisson = (Herisson) animal; // un animal devient un hérisson
+			System.out.println("Nombre d'aiguilles : " + herisson.getNbAiguilles());
+		}
+		
+		System.out.println("***********************************");
+		
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
